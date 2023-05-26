5c4b0526604068e73d44280fc5b6171445152e1e
Gbemiga Atolagbe <gbatolagbe@gmail.com>
2023-05-26 06:19:28 +0100

    changed json format

--- model/Score.js
+++ model/Score.js
@@ -1,20 +1,18 @@
 const mongoose = require("mongoose");
 
-const ScoreSchema = new mongoose.Schema([
-  {
-    name: {
-      type: String,
-      required: [true, "Must provide name"],
-      trim: true,
-      minlength: [3, "Must be 3 or more characters"],
-      maxlength: [25, "Can't be more than 25 characters"],
-    },
-    exactScore: {
-      type: Number,
-      min: [1, "Must be at least 1"],
-      max: [1000, "Can't exceed 1000"],
-    },
+const ScoreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Must provide name"],
+    trim: true,
+    minlength: [3, "Must be 3 or more characters"],
+    maxlength: [25, "Can't be more than 25 characters"],
   },
-]);
+  exactScore: {
+    type: Number,
+    min: [1, "Must be at least 1"],
+    max: [1000, "Can't exceed 1000"],
+  },
+});
 
 module.exports = mongoose.model("Score", ScoreSchema);
